1967b9e5706324cb1a2f2eddea49ba8e5328ec79
Xiang Lin <linxiangblcu@gmail.com>
2015-04-23 18:19:54 +0800

    add word stem

--- src/com/simpleplugin/SimpleSyntaxHighlighter.java
+++ src/com/simpleplugin/SimpleSyntaxHighlighter.java
@@ -22,7 +22,12 @@ public class SimpleSyntaxHighlighter extends SyntaxHighlighterBase {
 
     public static final TextAttributesKey RULE = createTextAttributesKey("SIMPLE_RULE", DefaultLanguageHighlighterColors.KEYWORD);
 
-    public static final TextAttributesKey STRING = createTextAttributesKey("SIMPLE_STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey FEATURE = createTextAttributesKey("SIMPLE_FEATURE", DefaultLanguageHighlighterColors.STRING);
+
+    public static final TextAttributesKey word = createTextAttributesKey("SIMPLE_word", DefaultLanguageHighlighterColors.LABEL);
+
+    public static final TextAttributesKey stem = createTextAttributesKey("SIMPLE_stem", DefaultLanguageHighlighterColors.LABEL);
+
     public static final TextAttributesKey COMMENT = createTextAttributesKey("SIMPLE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
 
     static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey("SIMPLE_BAD_CHARACTER",
@@ -30,10 +35,19 @@ public class SimpleSyntaxHighlighter extends SyntaxHighlighterBase {
 
 
     private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
-    private static final TextAttributesKey[] KEY_STRING = new TextAttributesKey[]{STRING};
+
+    private static final TextAttributesKey[] KEY_FEATURE = new TextAttributesKey[]{FEATURE};
+
     private static final TextAttributesKey[] KEY_RULE = new TextAttributesKey[]{RULE};
+
+    private static final TextAttributesKey[] KEY_WORD = new TextAttributesKey[]{word};
+
+    private static final TextAttributesKey[] KEY_STEM = new TextAttributesKey[]{stem};
+
     private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
     private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
 
     @NotNull
@@ -47,11 +61,15 @@ public class SimpleSyntaxHighlighter extends SyntaxHighlighterBase {
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
         if (tokenType.equals(SimpleTypes.SEPARATOR)) {
             return SEPARATOR_KEYS;
-        } else if (tokenType.equals(SimpleTypes.STRING)) {
-            return KEY_STRING;
+        } else if (tokenType.equals(SimpleTypes.FEATURE)) {
+            return KEY_FEATURE;
         } else if (tokenType.equals(SimpleTypes.RULE)) {
             return KEY_RULE;
-        } else if (tokenType.equals(SimpleTypes.COMMENT)) {
+        }else if (tokenType.equals(SimpleTypes.ONEWORD)) {
+            return KEY_WORD;
+        }else if (tokenType.equals(SimpleTypes.ONESTEM)) {
+            return KEY_STEM;
+        }else if (tokenType.equals(SimpleTypes.COMMENT)) {
             return COMMENT_KEYS;
         } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
             return BAD_CHAR_KEYS;
--- src/com/simpleplugin/simple.flex
+++ src/com/simpleplugin/simple.flex
@@ -21,13 +21,23 @@ WHITE_SPACE=[\ \t\f]
 END_OF_LINE_COMMENT=("#"|"!")[^\r\n]*
 
 SEPARATOR= "="
-STRING = [:letter:]*
+FEATURE = [:letter:]*
 RULE = [:letter:]*
+ONEWORD = [:letter:]*
+ONESTEM = [:letter:]*
+
 OR = "|"
 PLUS = "+"
-
+LCURLY = "{"
+RCURLY = "}"
+SINGLEQUOTATION = "'"
+DOUBLEQUOTATION = "\""
+NOT = "!"
 
 %state EXPRESSION
+%state WORD
+%state STEM
+%state FEATURE
 
 %%
 
@@ -37,18 +47,44 @@ PLUS = "+"
 
 <YYINITIAL> {SEPARATOR}                         { yybegin(EXPRESSION); return SimpleTypes.SEPARATOR; }
 
-<EXPRESSION>{CRLF}                              { yybegin(YYINITIAL); return SimpleTypes.CRLF; }
+<EXPRESSION>{SINGLEQUOTATION}                   { yybegin(STEM); return SimpleTypes.SINGLEQUOTATION;}
+
+<STEM>{SINGLEQUOTATION}                         { yybegin(EXPRESSION); return SimpleTypes.SINGLEQUOTATION;}
+
+<EXPRESSION>{DOUBLEQUOTATION}                   { yybegin(WORD); return SimpleTypes.DOUBLEQUOTATION;}
+
+<WORD>{DOUBLEQUOTATION}                        { yybegin(EXPRESSION); return SimpleTypes.DOUBLEQUOTATION;}
+
+<EXPRESSION> {FEATURE}                          { yybegin(EXPRESSION); return SimpleTypes.FEATURE; }
+
+<STEM> {ONESTEM}                                { yybegin(STEM); return SimpleTypes.ONESTEM; }
+
+<WORD> {ONEWORD}                               { yybegin(WORD); return SimpleTypes.ONEWORD; }
+
+//<EXPRESSION>{OR}                                { yybegin(EXPRESSION); return SimpleTypes.OR;}
+
+//<EXPRESSION>{PLUS}                              { yybegin(EXPRESSION); return SimpleTypes.PLUS;}
+
+<EXPRESSION>{LCURLY}                            { yybegin(EXPRESSION); return SimpleTypes.LCURLY;}
+
+<EXPRESSION>{RCURLY}                            { yybegin(EXPRESSION); return SimpleTypes.RCURLY;}
+
+//<EXPRESSION>{NOT}                               { yybegin(EXPRESSION); return SimpleTypes.NOT;}
+
+//<EXPRESSION>{CRLF}                              { yybegin(YYINITIAL); return SimpleTypes.CRLF; }
 
 <EXPRESSION> {WHITE_SPACE}+                     { yybegin(EXPRESSION); return TokenType.WHITE_SPACE; }
-
-<EXPRESSION> {STRING}                           { yybegin(EXPRESSION); return SimpleTypes.STRING; }
-
-<EXPRESSION>{OR}                                { yybegin(EXPRESSION); return SimpleTypes.OR;}
-
-<EXPRESSION>{PLUS}                              { yybegin(EXPRESSION); return SimpleTypes.PLUS;}
 
 {CRLF}                                          { yybegin(YYINITIAL); return SimpleTypes.CRLF; }
 
 {WHITE_SPACE}+                                  { return TokenType.WHITE_SPACE; }
 
+{OR}                                            { return SimpleTypes.OR;}
+
+{PLUS}                                          { return SimpleTypes.PLUS;}
+
+{NOT}                                           { return SimpleTypes.NOT;}
+
 .                                               { return TokenType.BAD_CHARACTER; }
+
+
